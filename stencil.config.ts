de15aa3fc337eb9479cfbd815bbff258d5751261
Aymen <labidiaymen@outlook.com>
2019-01-02 15:01:58 +0100

    start alert component

---
+++ stencil.config.ts
@@ -0,0 +1,18 @@
+import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
+
+export const config: Config = {
+  namespace: 'vaulab-src',
+  outputTargets: [
+    { type: 'dist' },
+    { type: 'docs' },
+    {
+      type: 'www',
+      serviceWorker: null // disable service workers
+    }
+  ],
+  plugins: [sass()],
+  bundles: [
+    { components: ['lab-alert'] },
+  ]
+};
